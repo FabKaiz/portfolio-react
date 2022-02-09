c760c743bc34273083c49f5f5d6407277db3a163
Fabien Chareun <cacrfabfr@gmail.com>
2022-02-09 14:05:39 +0100

    folder and files  structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import './App.css';
-
 import { About, Contact, Header, Portfolio, Project, Qualification, Skills } from './container'
 import { Navbar, Footer } from './components'
+import './App.css';
 
 function App() {
   return (
---
+++ src/constants/data.js
@@ -0,0 +1,1 @@
+// Here all my recent projects and quelifications
