a37dc844fa5ac9e481a837a4e0cee851e4c7ef87
Fabien Chareun <cacrfabfr@gmail.com>
2022-02-10 10:40:09 +0100

    navbar setup and added assets

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,63 +1,66 @@
 import React from 'react'
 
+import { BiHomeAlt } from 'react-icons/bi'
+import { AiOutlineUser } from 'react-icons/ai'
+
 import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <header class=" header" id="header">
-    <nav class="nav container">
-      <a href="#" class="nav__logo">
-        <div class="nav__logo-container">
+    <header className="header" id="header">
+    <nav className="nav container">
+      <a href="#home" className="nav__logo">
+        <div className="nav__logo-container">
           {/* <img src="assets/img/FC-logo.png" alt="Mon logo FC"> */}
           Fabien
         </div>
       </a>
-      <div class="nav__menu" id="nav-menu">
-        <ul class="nav__list grid">
-          <li class="nav__item">
-            <a href="#home" class="nav__link active-link">
-              <i class="uil uil-estate nav__icon"></i>
+      <div className="nav__menu" id="nav-menu">
+        <ul className="nav__list grid">
+          <li className="nav__item">
+            <a href="#home" className="nav__link active-link">
+              <BiHomeAlt className='nav__icon' />
               Accueil
             </a>
           </li>
-          <li class="nav__item">
-            <a href="#about" class="nav__link">
-              <i class="uil uil-user nav__icon"></i>
+          <li className="nav__item">
+            <a href="#about" className="nav__link">
+              <AiOutlineUser  className='nav__icon' />
               À propos
             </a>
           </li>
-          <li class="nav__item">
-            <a href="#skills" class="nav__link">
-              <i class="uil uil-file-info-alt nav__icon"></i>
+          <li className="nav__item">
+            <a href="#skills" className="nav__link">
+              <i className="uil uil-file-info-alt nav__icon"></i>
               Compétences
             </a>
           </li>
-          <li class="nav__item">
-            <a href="#services" class="nav__link">
-              <i class="uil uil-briefcase nav__icon"></i>
+          <li className="nav__item">
+            <a href="#services" className="nav__link">
+              <i className="uil uil-briefcase nav__icon"></i>
               Services
             </a>
           </li>
-          <li class="nav__item">
-            <a href="#portfolio" class="nav__link">
-              <i class="uil uil-scenery nav__icon"></i>
+          <li className="nav__item">
+            <a href="#portfolio" className="nav__link">
+              <i className="uil uil-scenery nav__icon"></i>
               Projet récent
             </a>
           </li>
-          <li class="nav__item">
-            <a href="#contact" class="nav__link">
-              <i class="uil uil-message nav__icon"></i>
+          <li className="nav__item">
+            <a href="#contact" className="nav__link">
+              <i className="uil uil-message nav__icon"></i>
               Me contacter
             </a>
           </li>
         </ul>
-        <i class="uil uil-times nav__close" id="nav-close"></i>
+        <i className="uil uil-times nav__close" id="nav-close"></i>
       </div>
-      <div class="nav__btns">
+      <div className="nav__btns">
         {/* <!-- Darkmode btn --> */}
-        <i class="uil uil-moon change-theme" id="theme-button"></i>
+        <i className="uil uil-moon change-theme" id="theme-button"></i>
         {/* <!-- Switch language btn --> */}
-        <span class="switch__lang" id="switch-lang">
+        <span className="switch__lang" id="switch-lang">
           EN
         </span>
         {/* <!-- <svg xmlns="http://www.w3.org/2000/svg" width="20" height="15" viewBox="0 0 640 480">
@@ -67,9 +70,9 @@ const Navbar = () => {
             <path fill="#f31830" d="M426.662 0H640v480H426.662z"/>
           </g>
         </svg> --> */}
-        <i class="uil change-theme" id="switch-lang"></i>
-        <div class="nav__toggle" id="nav-toggle">
-          <i class="uil uil-apps"></i>
+        <i className="uil change-theme" id="switch-lang"></i>
+        <div className="nav__toggle" id="nav-toggle">
+          <i className="uil uil-apps"></i>
         </div>
       </div>
     </nav>
