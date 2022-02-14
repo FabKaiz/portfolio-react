a04da0855bbb5aeede241422bdc7a47ff0a62862
Fabien Chareun <cacrfabfr@gmail.com>
2022-02-14 12:26:58 +0100

    header setup

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,22 +8,24 @@ import './Header.scss';
 
 const Header = () => {
   return (
-    <section className="home section" id="home">
-      <div className="home__container container">
-        <p>Je suis</p>
-        <h1>Fabien Chareun</h1>
-        <p>Développeur Web Front-end</p>
-        <Button
-          title='Me Contacter'
-          scrollTo='#contact'
-          logo={<BiSend />}
-        />
-        <div className="home__scroll">
-          <a href="#about" className="home__scroll-button">
-            <BsMouse />
-            <span className="home__scroll-name">Scroll-down</span>
-            <BiDownArrowAlt />
-          </a>
+    <section className="home" id="home">
+      <div className="home__container">
+        <div className="home__content">
+          <p>Je suis</p>
+          <h1>Fabien Chareun</h1>
+          <p>Développeur Web Front-end</p>
+          <Button
+            title='Me Contacter'
+            href='#contact'
+            logo={<BiSend />}
+          />
+          <div className="home__scroll">
+            <a href="#about" className="home__scroll-button">
+              <BsMouse />
+              <span className="home__scroll-name">Scroll-down</span>
+              <BiDownArrowAlt />
+            </a>
+          </div>
         </div>
       </div>
     </section>
