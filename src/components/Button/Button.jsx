3f8e135c2bf0bb4ab630104a4bbd6a9ad346102e
Fabien Chareun <cacrfabfr@gmail.com>
2022-02-14 11:30:10 +0100

    main Button setup

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Button.scss'
 
-const Button = ({ scrollTo, title, logo}) => {
+const Button = ({ scrollTo, title, logo }) => {
   return (
     <a href={scrollTo} className="button">
       {title} {logo}
