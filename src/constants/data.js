a7ba8dd4795cebe63f17dd63aac77c2a2ccea572
Fabien Chareun <cacrfabfr@gmail.com>
2022-02-16 10:58:30 +0100

    data setup

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,1 +1,24 @@
 // Here all my recent projects and qualifications
+import images from './images';
+
+const projects = [
+  {
+    title: 'Gerich restaurant responsive - React',
+    description: `Reproduction d'une maquette Figma d'un restaurant 100% responsive, réalisé avec React.`,
+    figmaLink: 'https://www.figma.com/file/yvClSI9AZBRX8UaaGEByF3/Modern-UI%2FUX%3A-Gericht?node-id=53%3A2',
+    projectSite: 'https://gerich-restaurant.netlify.app/',
+    projectRepo: 'https://github.com/FabKaiz/gerich-restaurant',
+    img: images.Port14
+  },
+  {
+    title: 'Modern UI/UX GPT-3 Website - Reac',
+    description: `Site static reprenant les design modernes des sites web <strong>GPT-3</strong>, Réalisé avec
+    <strong>React</strong>`,
+    figmaLink: 'https://www.figma.com/file/lz9lLpFHMxHm2odnwM3R0z/gpt3?node-id=0%3A1',
+    projectSite: 'https://gpt3-modern-react.netlify.app/',
+    projectRepo: 'https://github.com/FabKaiz/modern-gpt3-react',
+    img: images.Port13
+  },
+]
+
+export default { projects };
