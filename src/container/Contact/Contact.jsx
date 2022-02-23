29acc5a260ca441b87527a7d5495979039134675
Fabien Chareun <cacrfabfr@gmail.com>
2022-02-23 11:13:43 +0100

    contact setup  🛠

--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -8,7 +8,16 @@ const Contact = () => {
       <h2 className="section__title">Me contacter</h2>
       <span className="section__subtitle">Fabien Chareun</span>
 
+      <div className="contact__container">
 
+        <div className="contact__info">
+
+        </div>
+
+        <div className="contact__form">
+          
+        </div>
+      </div>
 
     </section>
   )
