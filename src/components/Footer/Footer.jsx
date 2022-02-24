76326eae00cd8148d6c3389e508efecf5a0afdd6
Fabien Chareun <cacrfabfr@gmail.com>
2022-02-24 11:04:49 +0100

    footer html setup

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,43 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="footer__container footer__bg grid">
+
+        <div className="footer__name">
+          <h1 className="footer__title">Fabien</h1>
+          <span className="footer__subtitle">Développeur Front-end</span>
+        </div>
+
+        <ul className="footer__links">
+          <li>
+            <a href="#skills">Compétences</a>
+          </li>
+          <li>
+            <a href="#recent-work">Projet récent</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+
+        <div className="footer__socials">
+          <a href="https://www.instagram.com/fab_citron" target="_blank" class="footer__social">
+            <i class="uil uil-instagram-alt"></i>
+          </a>
+          <a href="https://twitter.com/ChareunFabien" target="_blank" class="footer__social">
+            <i class="uil uil-twitter"></i>
+          </a>
+          <a href="https://github.com/FabKaiz" target="_blank" class="footer__social">
+            <i class="uil uil-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/fabien-chareun/" target="_blank" class="footer__social">
+            <i class="uil uil-linkedin"></i>
+          </a>
+        </div>
+
+      </div>
+    </footer>
   )
 }
 
