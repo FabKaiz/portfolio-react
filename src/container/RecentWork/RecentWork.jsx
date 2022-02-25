75072e42e5c39168debf903bd1c382fd9b340b2e
Fabien Chareun <cacrfabfr@gmail.com>
2022-02-25 12:40:06 +0100

    removed comment

--- src/container/RecentWork/RecentWork.jsx
+++ src/container/RecentWork/RecentWork.jsx
@@ -32,50 +32,6 @@ const RecentWork = () => {
       <div className="recent__work-container">
         {slicedRepos.map(renderRepo)}
       </div>
-
-      {/* SOLID DATA FOR DEV ONLY */}
-      {/* <div className="recent__work-container">
-        <div className="repo__content">
-          <div className="repo__container">
-            <h2 className="repo__title">
-              Responsive restaurant
-            </h2>
-            <p>Modern UI/UX Restaurant Landing Page Website</p>
-            <div className="repo__buttons">
-              <a href='https://github.com/FabKaiz/gerich-restaurant' target="_blank" rel="noreferrer">
-                Voir le repo
-              </a>
-              <a href='https://github.com/FabKaiz/gerich-restaurant' target="_blank" rel="noreferrer">
-                Voir le site
-              </a>
-            </div>
-            <div className="line"></div>
-            <span className={`repo__language-tag Vue`} >Vue</span>
-          </div>
-        </div>
-
-        <div className="repo__content">
-          <div className="repo__container">
-            <h2 className="repo__title">
-              Responsive restaurant
-            </h2>
-            <p>Modern UI/UX Restaurant Landing Page Website</p>
-            <div className="repo__buttons">
-              <a href='https://github.com/FabKaiz/gerich-restaurant' target="_blank" rel="noreferrer">
-                Voir le repo
-              </a>
-              <a href='https://github.com/FabKaiz/gerich-restaurant' target="_blank" rel="noreferrer">
-                Voir le site
-              </a>
-            </div>
-            <div className="line"></div>
-            <span className={`repo__language-tag Ruby`} >Ruby</span>
-          </div>
-        </div>
-
-      </div> */}
-
-
     </section>
   )
 }
