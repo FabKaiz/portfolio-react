789d2dbf4374f9c8d18b6c8ab72228e716021d6c
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-07 14:41:13 +0100

    remove "see website" if there is no website

--- src/components/RepoCard/RepoCard.jsx
+++ src/components/RepoCard/RepoCard.jsx
@@ -23,9 +23,12 @@ const RepoCard = ({ repo }) => {
         <a href={repo.html_url} target="_blank" rel="noreferrer">
           Voir le repo
         </a>
-        <a href={repo.homepage} target="_blank" rel="noreferrer">
-          Voir le site
-        </a>
+        {repo.homepage && (
+          <a href={repo.homepage} target="_blank" rel="noreferrer">
+            Voir le site
+          </a>
+        )}
+        
       </div>
 
       <div className="line"></div>
