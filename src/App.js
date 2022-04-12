26ef029a3fa6e592c987edb01f0cf826fbdd72a8
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-12 15:34:42 +0200

    added google analytics

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ import {
 } from "./container";
 import { Navbar, Footer, ParticleBackground } from "./components";
 import "./App.scss";
-
+import ReactGa from "react-ga";
+import { useEffect } from "react";
 
 function App() {
+  // Google Analytics
+  useEffect(() => {
+    ReactGa.initialize(process.env.REACT_APP_GA_TRACKING_KEY);
+    ReactGa.pageview('/')
+  }, [])
+  
   return (
     <div className="App">
       <Navbar />
